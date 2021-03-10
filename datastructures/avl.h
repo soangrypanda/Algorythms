7c6a47b669ccd9db20822ba2d091a36389865d1e
soangrypanda <57560030+soangrypanda@users.noreply.github.com>
2021-03-10 15:10:31 +0300

    Final iteration.

--- datastructures/avl.h
+++ datastructures/avl.h
@@ -2,32 +2,34 @@
 #define AVL_TREE_SENTRY
 
 
-#define INPUT_SIZE	100
-#define INPUT_MIN	0
-#define INPUT_MAX	100
+#include <inttypes.h>
+#include <stddef.h>
 
 #define	AVL_NODE			struct node
-#define AVL_NODE_P 			AVL_NODE *
+#define AVL_NODE_P 	  AVL_NODE *
 #define AVL_TREE			AVL_NODE_P *
-
-
+#define key_t			    int32_t
+#define PRIKEY				PRId32
 
 struct node;
 
-#define		new_avl_tree(X)	AVL_TREE X = init_avl_tree(X)
-AVL_TREE	init_avl_tree	(AVL_TREE tree);
-void   		insert_node		(AVL_TREE tree, int32_t d);
-void   		delete_node		(AVL_TREE tree, AVL_NODE_P np);
-void		delete_tree		(AVL_TREE tree);
-AVL_NODE_P	pop_node		(AVL_TREE tree, AVL_NODE_P np);
-AVL_NODE_P	get_max			(AVL_TREE tree);
-AVL_NODE_P	get_min			(AVL_TREE tree);
-AVL_NODE_P	get_node		(AVL_TREE tree, int32_t key);
-AVL_NODE_P	find_next_larger(AVL_NODE_P np);
-AVL_NODE_P	find_next_lesser(AVL_NODE_P np);
-/* print_tree flags are PLAIN, VERBOSE. */
-void		print_tree		(AVL_TREE tree, int32_t flag);
+enum print_tree_flags {PLAIN, VERBOSE};
 
+#define		  new_avl_tree(X)	  AVL_TREE X = init_avl_tree(X)
+AVL_TREE	  init_avl_tree	    (AVL_TREE tree);
+void   		  insert_node		    (AVL_TREE tree, key_t key, void *data, 
+							                uint32_t data_size, char *inserted);
+void   		  delete_node		    (AVL_TREE tree, AVL_NODE_P np);
+AVL_TREE	  delete_tree		    (AVL_TREE tree);
+AVL_NODE_P	pop_node		      (AVL_TREE tree, AVL_NODE_P np);
+AVL_NODE_P	get_max			      (AVL_TREE tree);
+AVL_NODE_P	get_min			      (AVL_TREE tree);
+AVL_NODE_P	get_node		      (AVL_TREE tree, key_t key);
+AVL_NODE_P	find_next_larger  (AVL_NODE_P np);
+AVL_NODE_P	find_next_lesser  (AVL_NODE_P np);
+void		    print_tree		    (AVL_TREE tree, int32_t flag);
+void		    check_invariant	  (AVL_TREE tree);
+void		    insert_random_data(AVL_TREE tree, size_t size, int32_t min, int32_t max);
 
 
 #endif
