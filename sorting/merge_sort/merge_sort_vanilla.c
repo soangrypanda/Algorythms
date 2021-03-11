20d06f028aaad4395b643df793c5346744c4397f
soangrypanda <soangrypanda at protonmail dot com>
2021-03-11 17:24:24 +0300

    cleanup

--- sorting/merge_sort/merge_sort_vanilla.c
+++ sorting/merge_sort/merge_sort_vanilla.c
@@ -1,61 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "merge_sort_vanilla.h"
 
-void merge_sort(int *arr, int strt, int end);
-void merge(int *arr, int strt, int mid, int end);
-void print_arr(int *arr, int arr_len);
 
-int main(void) {
-  int arr[] = { 8, 1, 10, 54, 2, 0, -3, 5, 70, 60, 11, 4 };
-  int arr_len = sizeof(arr) / sizeof(arr[0]); 
-  print_arr(arr, arr_len);
-  merge_sort(arr, 0, arr_len);
-  print_arr(arr, arr_len);
+void merge_sort	(int32_t *arr, uint32_t strt, uint32_t len);
+void merge	(int32_t *arr, uint32_t strt, uint32_t mid, uint32_t len);
+
+
+void merge_sort(int32_t *arr, uint32_t strt, uint32_t len) {
+	if (len - strt > 1) {
+	/* avoid potential arithmetic overflow on start+end
+     	 * for more info see:
+     	 * https://ai.googleblog.com/2006/06/extra-extra-read-all-about-it-nearly.html
+     	 * https://thebittheories.com/the-curious-case-of-binary-search-the-famous-bug-that-remained-undetected-for-20-years-973e89fc212
+	 */
+		uint32_t mid = strt + (len - strt) / 2;
+		merge_sort(arr, strt, mid);
+		merge_sort(arr, mid, len);
+		merge(arr, strt, mid, len); 
+	}
 }
 
-void print_arr(int *arr, int arr_len) {
-  for (int i = 0; i < arr_len; i++) {
-    printf("%i ", arr[i]);
-  }
-  printf("\n");
-}
+void merge(int32_t *arr, uint32_t strt, uint32_t mid, uint32_t len) {
+	int32_t *tmp_arr = malloc(sizeof(*tmp_arr) * (len - strt));
+	uint32_t i = strt;
+	uint32_t j = 0;
+ 	uint32_t k = mid;
 
-void merge_sort(int *arr, int strt, int end) {
-  if (end - strt > 1) {
-    // avoid potential arithmetic overflow on start+end
-    //for more info see:
-    //https://ai.googleblog.com/2006/06/extra-extra-read-all-about-it-nearly.html
-    //https://thebittheories.com/the-curious-case-of-binary-search-the-famous-bug-that-remained-undetected-for-20-years-973e89fc212
-    int mid = strt + (end - strt) / 2;
-    merge_sort(arr, strt, mid);
-    merge_sort(arr, mid, end);
-    merge(arr, strt, mid, end); 
-  }
-}
+	while (i < mid && k < len) {
+ 		if (arr[i] <= arr[k])	tmp_arr[j++] = arr[i++];
+		else			tmp_arr[j++] = arr[k++];
+	}
+  	
+	while (i < mid) {
+		tmp_arr[j++] = arr[i++];
+	}
+	/* copying the remaining elements from the right half is not needed as they
+	 * are already in the proper place
+	while (k < end) {
+		tmp_arr[j++] = arr[k++];
+	} 
+	 */
+	for (size_t m = 0; m < j; m++) {
+		arr[strt + m] = tmp_arr[m];
+	}
 
-void merge(int *arr, int strt, int mid, int end) {
-  int *tmp_arr = malloc(sizeof(*tmp_arr) * (end - strt));
-  //int tmp_arr[end - strt];
-  int i = strt;
-  int j = 0;
-  int k = mid;
-  while (i < mid && k < end) {
-    if (arr[i] <= arr[k]) {
-      tmp_arr[j++] = arr[i++];
-    } else {
-      tmp_arr[j++] = arr[k++];
-    }
-  }
-  while (i < mid) {
-      tmp_arr[j++] = arr[i++];
-  }
-  // copying the remaining elements from the right half is not needed as they
-  // are already in the proper place
-  //while (k < end) {
-  //    tmp_arr[j++] = arr[k++];
-  //}
-  for (int m = 0; m < j; m++) {
-    arr[strt + m] = tmp_arr[m];
-  }
-  free(tmp_arr);
+	free(tmp_arr);
 }
---
+++ sorting/merge_sort/merge_sort_vanilla.h
@@ -0,0 +1,14 @@
+#ifndef MERGE_SORT_VANILLA_SENTRY
+#define MERGE_SORT_VANILLA_SENTRY
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+
+void merge_sort	(int32_t *arr, uint32_t strt, uint32_t len);
+void merge	(int32_t *arr, uint32_t strt, uint32_t mid, uint32_t len);
+
+
+#endif
