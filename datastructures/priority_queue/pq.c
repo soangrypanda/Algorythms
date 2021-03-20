2681b2cc887a9a87bbb64cd55628fec3e8d5ce29
soangrypanda <soangrypanda at protonmail dot com>
2021-03-20 21:24:01 +0300

    REFAC: brushing up, introduce typedef instead of #define for types.

--- datastructures/priority_queue/pq.c
+++ datastructures/priority_queue/pq.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <math.h>
 
 #include "pq.h"
 
 struct pq_data {
 	void*	data;
 	size_t	data_size;
-	key_t	priority;
+	pri_t	priority;
 };
 
 struct pq {
@@ -19,13 +18,34 @@ struct pq {
 };
 
 
+/**********************************
+ * INTERNAL INTERFACE DESCRIPTION *
+ **********************************/
+
+/*
+ * PQ DATA
+ */
+static PQ_DATA_P	create_data		(void *data, size_t data_size, pri_t priority);
+/*
+ * PQ
+ */
+static PQ_DATA_P*	heap_sort_do	(PQ_P pqp);
+static PQ_P			max_heapify		(PQ_P pqp, size_t index);
+/*
+ * MISCELANEOUS INTERNAL INTERFACE
+ */
+static int32_t		get_rand_num	(int32_t l, int32_t h);
+static void			init_rand		(void);
 
 
-PQ_DATA_P* heap_sort_do(PQ_P pqp);
-
-
-PQ_DATA_P
-create_data(void *data, size_t data_size, key_t priority)
+/*
+ * PQ DATA
+ */
+ 
+ /* PQ DATA INTERNAL IMPLEMENTATIONS */
+ 
+static PQ_DATA_P
+create_data(void *data, size_t data_size, pri_t priority)
 {
 	PQ_DATA_P data_p = calloc(1, sizeof(*data_p));
 	if(NULL == data_p) {
@@ -41,8 +61,11 @@ create_data(void *data, size_t data_size, key_t priority)
 }
 
 
+/*
+ * PQ
+ */
 
-
+/* PQ EXTERNAL IMPLEMENTATIONS */
 
 PQ_P		
 init_pq (size_t size, PQ_DATA_P *pq_a)
@@ -73,8 +96,23 @@ init_pq (size_t size, PQ_DATA_P *pq_a)
 	return pq;
 }
 
+void
+delete_pq_do(PQ_P pqp)
+{
+	PQ_DATA_P *pq_a		= pqp->pq;
+	size_t len			= pqp->last_free;
+	
+	for(size_t i = 0; i < len; ++i) {
+		free(pq_a[i]->data);
+		free(pq_a[i]);
+	}
+	
+	free(pq_a);
+	free(pqp);
+}
+
 PQ_P
-insert_into_max_heap(PQ_P pqp, void *data, size_t data_size, key_t priority)
+insert_into_max_heap(PQ_P pqp, void *data, size_t data_size, pri_t priority)
 {
 	printf("\n   ENTERING insert_into_max_heap\n");
 	PQ_DATA_P *pq_arr	= pqp->pq;
@@ -107,6 +145,28 @@ insert_into_max_heap(PQ_P pqp, void *data, size_t data_size, key_t priority)
 	return pqp;
 }
 
+PQ_DATA_P
+peek_pq(PQ_P pqp) 
+{
+	return pqp->pq[0];
+}
+
+PQ_DATA_P
+pull_high_elem(PQ_P pqp)
+{
+	PQ_DATA_P *pq_a	= pqp->pq;
+	size_t last_e_i	= pqp->last_free - 1;
+	PQ_DATA_P ret	= pq_a[0];
+	
+	pq_a[0]			= pq_a[last_e_i];
+	pq_a[last_e_i]	= NULL;
+	pqp->last_free--;
+	
+	max_heapify(pqp, 0);
+	
+	return ret;
+}
+
 PQ_P
 build_max_heap(PQ_DATA_P *array, size_t arr_len)
 {
@@ -122,26 +182,6 @@ build_max_heap(PQ_DATA_P *array, size_t arr_len)
 	return pqp;
 }
 
-PQ_DATA_P
-peek_pq(PQ_P pqp) {
-	return pqp->pq[0];
-}
-
-PQ_DATA_P
-pull_high_elem(PQ_P pqp) {
-	PQ_DATA_P *pq_a	= pqp->pq;
-	size_t last_e_i	= pqp->last_free - 1;
-	PQ_DATA_P ret	= pq_a[0];
-	
-	pq_a[0]			= pq_a[last_e_i];
-	pq_a[last_e_i]	= NULL;
-	pqp->last_free--;
-	
-	max_heapify(pqp, 0);
-	
-	return ret;
-}
-
 PQ_DATA_P*
 heap_sort(PQ_DATA_P *array, size_t arr_len)
 {
@@ -149,22 +189,9 @@ heap_sort(PQ_DATA_P *array, size_t arr_len)
 	return heap_sort_do(pqp);
 }
 
-void
-delete_pq_do(PQ_P pqp)
-{
-	PQ_DATA_P *pq_a		= pqp->pq;
-	size_t len			= pqp->last_free;
-	
-	for(size_t i = 0; i < len; ++i) {
-		free(pq_a[i]->data);
-		free(pq_a[i]);
-	}
-	
-	free(pq_a);
-	free(pqp);
-}
+/* PQ DATA INTERNAL IMPLEMENTATIONS */
 
-PQ_DATA_P*
+static PQ_DATA_P*
 heap_sort_do(PQ_P pqp)
 {
 	#ifdef DEBUG
@@ -189,15 +216,7 @@ heap_sort_do(PQ_P pqp)
 	return pq_a;
 }
 
-
-
-
-
-
-
-
-
-PQ_P
+static PQ_P
 max_heapify(PQ_P pqp, size_t index)
 {
 	#ifdef DEBUG
@@ -213,9 +232,9 @@ max_heapify(PQ_P pqp, size_t index)
 		size_t ic_one	= i * 2 + 1;
 		size_t ic_two	= i * 2 + 2;
 		
-		key_t p_one		= ic_one < lf ? data_arr[ic_one]->priority : MIN_PRIOR;
-		key_t p_two		= ic_two < lf ? data_arr[ic_two]->priority : MIN_PRIOR;
-		key_t p_i		= data_arr[i]->priority;
+		pri_t p_one		= ic_one < lf ? data_arr[ic_one]->priority : MIN_PRIOR;
+		pri_t p_two		= ic_two < lf ? data_arr[ic_two]->priority : MIN_PRIOR;
+		pri_t p_i		= data_arr[i]->priority;
 		
 		size_t ic_max	= (p_one >  p_two) * ic_one + 
 						  (p_two >  p_one) * ic_two + 
@@ -223,7 +242,7 @@ max_heapify(PQ_P pqp, size_t index)
 		
 		#ifdef DEBUG
 				printf(	"i is %zu and lf is %zu and ic_max is %zu "
-						"and ic_one is %zu(%"PRIKEY") and ic_two is %zu(%"PRIKEY")\n", 
+						"and ic_one is %zu(%"PRIPRI") and ic_two is %zu(%"PRIPRI")\n", 
 							i, lf, ic_max, ic_one, p_one, ic_two, p_two);
 		#endif
 		
@@ -242,12 +261,15 @@ max_heapify(PQ_P pqp, size_t index)
 }
 
 
+/*
+ * MISCELANEOUS IMPLEMENTATIONS
+ */
 
 
 /* PRINT PQ */
 
 void
-print_pq_old(PQ_P pqp)
+print_pq(PQ_P pqp)
 {
 	if(pqp == NULL) {
 		printf("No PQ to print!\n");
@@ -267,7 +289,7 @@ print_pq_old(PQ_P pqp)
 			next_level *= 2;
 		}
 		
-		printf("%" PRIKEY " ", data_arr[i]->priority);
+		printf("%" PRIPRI " ", data_arr[i]->priority);
 		cur_level++;	
 	}
 	
@@ -275,60 +297,11 @@ print_pq_old(PQ_P pqp)
 	printf("\n");
 }
 
-void
-print_pq(PQ_P pqp)
-{
-	if(pqp == NULL) {
-		printf("No PQ to print!\n");
-		return;
-	}
-	
-	printf("Printing PQ...\n");
-	
-	PQ_DATA_P	*arr		= pqp->pq;
-	size_t		elem_no		= pqp->last_free;
-	uint32_t	max_level	= log2(elem_no) + 1;
-	uint32_t	level		= 1;
-	uint32_t	level_len	= pow(2, max_level - 1);
-	uint32_t	offset		= level_len / 2 - 1;
-	uint32_t	prev_offset = offset;
-	
-	uint32_t	elem_at_level = 1;
-	
-	printf("max level - %u\n", max_level);
-	for(size_t i = 0; i < max_level; ++i) {
-		
-		for(size_t j = 0; j < offset; ++j) 
-			printf(" ");
-			
-		
-		for(size_t j = 0; j < elem_at_level; ++j) {
-			//int32_t printed = printf("%" PRIKEY, arr[i+j]->priority);
-			int32_t printed = printf("%s", arr[i+j]->data);
-			printed = printed > 1 ? printed -= 1 : printed;
-			int32_t blanks_to_print = 	prev_offset > printed ? 
-										prev_offset - printed : 0;
-			if(offset == 0) blanks_to_print = 1;
-			for(size_t c = 0; c < blanks_to_print; ++c) 
-				printf(" ");
-		}
-		
-		prev_offset		= offset;
-		elem_at_level	*= 2;
-		offset			/= 2;
-		printf("\n");
-	}
-	printf("\n");
-	
-	
-}
 /* RANDOM FUNCTION */
 
 /* using a normal random function is not a purporse of
  * this educational module. don't use it in practice */
 unsigned int seed;
-static int32_t get_rand_num(int32_t l, int32_t h);
-static void init_rand();
 
 void
 insert_random_data(PQ_P pqp, size_t size, int32_t min, int32_t max)
@@ -338,22 +311,22 @@ insert_random_data(PQ_P pqp, size_t size, int32_t min, int32_t max)
 	if(max==0) max = size;
 	
 	size_t input_size	= size;
-	key_t *inp_nums		= calloc(1, input_size * sizeof(*inp_nums));
+	pri_t *inp_nums		= calloc(1, input_size * sizeof(*inp_nums));
 	
 	/* one good input set */
 	//int32_t inp_nums[] = {3, 66, 84, 87, 58, 1, 26, 52, 14, 37};
 	//uint32_t input_size = 10;
 	for(size_t i = 0; i < input_size; ++i) {
 		inp_nums[i] 	= get_rand_num(min, max);
-		key_t priority	= inp_nums[i];
+		pri_t priority	= inp_nums[i];
 		
 		#ifdef DEBUG
-			printf("rand num is %" PRIKEY "\n", priority);
+			printf("rand num is %" PRIPRI "\n", priority);
 		#endif
 
-		int32_t dummy_len = snprintf(NULL, 0, "%" PRIKEY, priority) + 1;
+		int32_t dummy_len = snprintf(NULL, 0, "%" PRIPRI, priority) + 1;
 		char *	dummy	  = calloc(1, dummy_len);
-		snprintf(dummy, dummy_len, "%" PRIKEY, priority);
+		snprintf(dummy, dummy_len, "%" PRIPRI, priority);
 		
 		#ifdef DEBUG
 			printf("dummy data is %s\n", dummy);
@@ -374,15 +347,15 @@ get_random_pqpdata_arr(size_t size, int32_t min, int32_t max)
 	PQ_DATA_P *array	= calloc(size, sizeof(*array));
 	
 	for(size_t i = 0; i < size; ++i) {
-		key_t priority 	= get_rand_num(min, max);
+		pri_t priority 	= get_rand_num(min, max);
 		
 		#ifdef DEBUG
-			printf("rand num is %" PRIKEY "\n", priority);
+			printf("rand num is %" PRIPRI "\n", priority);
 		#endif
 
-		int32_t dummy_len	= snprintf(NULL, 0, "%" PRIKEY, priority) + 1;
+		int32_t dummy_len	= snprintf(NULL, 0, "%" PRIPRI, priority) + 1;
 		char *	dummy		= calloc(1, dummy_len);
-		snprintf(dummy, dummy_len, "%" PRIKEY, priority);
+		snprintf(dummy, dummy_len, "%" PRIPRI, priority);
 		
 		#ifdef DEBUG
 			printf("dummy data is %s\n", dummy);
@@ -395,7 +368,7 @@ get_random_pqpdata_arr(size_t size, int32_t min, int32_t max)
 }
 
 static void
-init_rand() 
+init_rand(void) 
 {
 	seed = time(NULL);
 }
@@ -404,43 +377,4 @@ static int32_t
 get_rand_num(int32_t l, int32_t h)
 {
 	return l + rand_r(&seed) % h;
-}
-
-
-
-
-
-
-int main(void)
-{
-	size_t	len = 100;
-	int32_t	min = 0;
-	int32_t	max = 100;
-	
-	new_pq(pq, 0);
-	
-	insert_random_data(pq, len, min, max);
-
-	
-	//PQ_DATA_P *pq_a = get_random_pqpdata_arr(len, min, max);
-	//PQ_P pq			= build_max_heap(pq_a, len);
-	//pq_a			= heap_sort(pq_a, len);
-	
-	print_pq_old(pq);
-	//printf("%"PRIKEY"\n", peek_pq(pq)->priority);
-	//printf("%"PRIKEY"\n", pull_high_elem(pq)->priority);
-	//print_pq_old(pq);
-	
-	//size_t len	= pq->last_free;
-	//pq_a			= heap_sort_do(pq);
-	//printf("Printing array\n");
-	//for(size_t i = 0; i < len; ++i) {
-	//	printf("%" PRIKEY " ", pq_a[i]->priority);
-	//}
-	//printf("\n");
-	
-	delete_pq(pq);
-	print_pq(pq);
-	
-	
 }
--- datastructures/priority_queue/pq.h
+++ datastructures/priority_queue/pq.h
@@ -1,17 +1,14 @@
 #include <inttypes.h>
 
 
-#define	PQ_DATA		struct pq_data
-#define PQ_DATA_P	PQ_DATA *
-#define	PQ			struct pq
-#define	PQ_P		PQ*
-#define key_t		uint32_t
-#define PRIKEY		PRIu32
-#define	MIN_PRIOR	0
-#define DEF_PQ_SIZE	100
-
-struct pq_data;
-struct pq;
+typedef	struct pq_data 	PQ_DATA;
+typedef	PQ_DATA *		PQ_DATA_P;
+typedef	struct pq		PQ;
+typedef	PQ*				PQ_P;
+typedef	uint32_t		pri_t;
+#define	PRIPRI			PRIu32
+#define	MIN_PRIOR		0
+#define	DEF_PQ_SIZE		100
 
 
 #define		new_pq(pq, size)	PQ_P pq = init_pq((size), NULL)
@@ -19,12 +16,14 @@ PQ_P		init_pq				(size_t size, PQ_DATA_P *pq_a);
 #define		delete_pq(pq)		do { delete_pq_do(pq); pq = NULL; } while(0)
 void		delete_pq_do		(PQ_P pqp);
 
+PQ_P		insert_into_max_heap(PQ_P pqp, void *data, size_t data_size, pri_t priority);
 PQ_P		build_max_heap		(PQ_DATA_P *array, size_t arr_len);
-
-PQ_P		insert_into_max_heap(PQ_P pqp, void *data, size_t data_size, key_t priority);
 PQ_DATA_P	peek_pq				(PQ_P pqp);
+PQ_DATA_P	pull_high_elem		(PQ_P pqp);
+PQ_P		build_max_heap		(PQ_DATA_P *array, size_t arr_len);
+PQ_DATA_P*	heap_sort			(PQ_DATA_P *array, size_t arr_len);
 
-char		is_empty			(PQ_P pqp);
+void		print_pq			(PQ_P pqp);
 
-
-PQ_P		max_heapify		(PQ_P pqp, size_t index);
+void		insert_random_data	(PQ_P pqp, size_t size, int32_t min, int32_t max);
+PQ_DATA_P*	get_random_pqpdata_arr(size_t size, int32_t min, int32_t max);
