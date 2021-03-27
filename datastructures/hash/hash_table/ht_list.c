a414db4fb026994914de3377b37eca6d49c329d3
soangrypanda <soangrypanda at protonmail dot com>
2021-03-27 16:35:12 +0300

    FIX: pop_item() can decrease ht size if n_elem * HT_DECREASE_THRESHOLD <= len.

--- datastructures/hash/hash_table/ht_list.c
+++ datastructures/hash/hash_table/ht_list.c
@@ -19,9 +19,43 @@ struct ht_item {
     size_t          d_size;
 };
 
-static HT_ITEM_S_P * find_place_in_slot(HT_ITEM_S_P *pp, char *key, char *ret);
-static void init_rand(void);
-static int32_t get_rand_num(int32_t l, int32_t h);
+enum { HT_DECREASE_TRESHOLD = 3 }; 
+
+/**********************************
+ * INTERNAL INTERFACE DESCRIPTION *
+ **********************************/
+ 
+/* HT_ITEM */
+static HT_ITEM_S_P  create_ht_item      (void *data, size_t d_size, char *key);
+
+/* HT */
+static void         delete_ht_arr       (HT_ITEM_S_P *arr, size_t arr_len);
+static void         delete_ht_list      (HT_ITEM_S_P list);
+static HT_ITEM_S_P* find_place_in_slot  (HT_ITEM_S_P *pp, char *key, char *ret);
+static signed char  insert_into_slot    (HT_ITEM_S_P *ht_arr, void *data, size_t d_size, 
+                                        char *key, size_t h_i);
+static void         replace_data        (HT_ITEM_S_P item, void *data, size_t d_size);
+static void         change_ht_len       (HT ht, int change_how);
+enum 									{ INCREASE_HT, DECREASE_HT };
+
+/* MISCELANEOUS */
+static char         is_prime            (size_t x);
+static size_t       next_prime          (size_t x);
+static size_t       hash                (char *key, uint32_t n);
+static void         prehash             (char *key, uint_least64_t *prehash);
+static void         init_rand           (void);
+static int32_t      get_rand_num        (int32_t l, int32_t h);
+
+
+/*****************************
+ * INTERFACE IMPLEMENTATIONS *
+ *****************************/
+ 
+/* 
+ * HT_ITEM
+ */
+
+/* HT_ITEM INTERNAL IMPLEMENTATIONS */
 
 static HT_ITEM_S_P
 create_ht_item(void *data, size_t d_size, char *key)
@@ -38,41 +72,11 @@ create_ht_item(void *data, size_t d_size, char *key)
     lnsp->next      = NULL;
 }
 
-void
-delete_ht_list(HT_ITEM_S_P list)
-{
-    while(list != NULL) {
-        HT_ITEM_S_P next = list->next;
-        free(list->data);
-        free(list->key);
-        free(list);
-        list = next;
-    }
-}
-
-void
-delete_ht_arr(HT_ITEM_S_P *arr, size_t arr_len)
-{
-    for(size_t i = 0; i < arr_len; ++i) 
-        delete_ht_list(arr[i]);
-    free(arr);
-}
-
-HT_ITEM_S_P
-pop_item(HT ht, char *key)
-{
-    HT_ITEM_S_P *arr    = ht->arr;
-    size_t h_i          = hash(key, ht->len);
-    HT_ITEM_S_P *pp     = &arr[h_i];
-    pp                  = find_place_in_slot(pp, key, NULL);
-
-    HT_ITEM_S_P ret     = *pp;
-    if(ret != NULL) {
-        *pp = ret->next; 
-    }
-    
-    return ret;
-}
+/*
+ * HT
+ */
+ 
+/* HT EXTERNAL IMPLEMENTATIONS */
 
 HT
 init_hash_table(size_t len)
@@ -154,7 +158,74 @@ find_item_by_key(HT ht, char *key)
     return *pp;
 }
 
-HT_ITEM_S_P *
+HT_ITEM_S_P
+pop_item(HT ht, char *key)
+{
+    HT_ITEM_S_P *arr    = ht->arr;
+    size_t len			= ht->len;
+    size_t h_i          = hash(key, len);
+    HT_ITEM_S_P *pp     = &arr[h_i];
+    pp                  = find_place_in_slot(pp, key, NULL);
+
+    HT_ITEM_S_P ret     = *pp;
+    if(ret != NULL) {
+        *pp = ret->next;
+        ht->n_elem--; 
+    }
+    
+    if(ht->n_elem * HT_DECREASE_TRESHOLD <= len) {
+    	change_ht_len(ht, DECREASE_HT);
+    }
+    
+    return ret;
+}
+
+void
+print_ht(HT ht)
+{
+    if(ht != NULL)
+        printf("Printing ht...\n");
+    else {
+        printf("No ht to print!\n");
+        return;
+    }
+
+    HT_ITEM_S_P *arr    = ht->arr;
+    size_t ht_len       = ht->len;
+
+    for(size_t i = 0; i < ht_len; ++i) {
+        printf("\t%zu \n", i);
+        HT_ITEM_S_P i_i = arr[i];
+        while(i_i != NULL) {
+            printf("\tkey is %s and data is %s\n", i_i->key, i_i->data);
+            i_i = i_i->next;
+        }
+    }
+}
+
+/* HT INTERNAL IMPLEMENTATIONS */
+
+static void
+delete_ht_arr(HT_ITEM_S_P *arr, size_t arr_len)
+{
+    for(size_t i = 0; i < arr_len; ++i) 
+        delete_ht_list(arr[i]);
+    free(arr);
+}
+
+static void
+delete_ht_list(HT_ITEM_S_P list)
+{
+    while(list != NULL) {
+        HT_ITEM_S_P next = list->next;
+        free(list->data);
+        free(list->key);
+        free(list);
+        list = next;
+    }
+}
+
+static HT_ITEM_S_P *
 find_place_in_slot(HT_ITEM_S_P *pp, char *key, char *ret)
 {
     char status = 0;
@@ -174,15 +245,7 @@ find_place_in_slot(HT_ITEM_S_P *pp, char *key, char *ret)
     return pp;
 }
 
-void
-replace_data(HT_ITEM_S_P item, void *data, size_t d_size)
-{
-    free(item->data);
-    item->data      = data;
-    item->d_size    = d_size;
-}
-
-signed char
+static signed char
 insert_into_slot(HT_ITEM_S_P *ht_arr, void *data, size_t d_size, char *key, size_t h_i)
 {
     HT_ITEM_S_P *pp      = &ht_arr[h_i]; 
@@ -202,7 +265,16 @@ insert_into_slot(HT_ITEM_S_P *ht_arr, void *data, size_t d_size, char *key, size
     return ret;
 }
 
-void
+static void
+replace_data(HT_ITEM_S_P item, void *data, size_t d_size)
+{
+    free(item->data);
+    item->data      = data;
+    item->d_size    = d_size;
+}
+
+
+static void
 change_ht_len(HT ht, int change_how)
 {
     #ifdef DEBUG
@@ -247,95 +319,11 @@ change_ht_len(HT ht, int change_how)
     free(arr);
 }
 
-char
-is_prime(size_t x)
-{
-    for (size_t i = 3; i < x; i += 2)
-    {
-        size_t p_sqrt = x / i;
-        if (p_sqrt < i)
-            return 1;
-        if(p_sqrt * i == x)
-            return 0;
-    }
-    return 1;
-}
-
-size_t
-next_prime(size_t x)
-{
-    #ifdef DEBUG
-        printf("\nEntering next_prime with value %zu\n", x);
-    #endif
-
-    if (x < 2)      return 2;
-
-    if (!(x & 1))   ++x;
-    else            x += 2;
-
-    for (; !is_prime(x); x += 2)    ;
-
-    #ifdef DEBUG
-        printf("\tNext prime is %zu\n\n", x);
-    #endif
-
-    return x;
-}
-
-size_t
-hash(char *key, uint32_t n)
-{
-    uint_least64_t pre_h = 0;
-    prehash(key, &pre_h);
-
-    #ifdef DEBUG
-        printf("\tprehash value is %" PRIuLEAST64 ".\n", pre_h);
-    #endif
-
-    return pre_h % n;
-}
-
-void
-prehash(char *key, uint_least64_t *prehash)
-{
-    size_t key_len      = strlen(key);
-    uint_least64_t sum  = 0;
-    char prev           = 0;
-    char next           = 0;
-    size_t i            = 0;
-    for( ; i < key_len - 1; ++i) {
-        next = key[i+1];
-        if(i % 2 == 0)  sum += (key[i] + prev) * next; 
-        else            sum += key[i] * prev + next;
-        prev = key[i];
-    }
-    sum += key[i] + sum & next;
-
-    *prehash            = sum;
-}
-
-void
-print_ht(HT ht)
-{
-    if(ht != NULL)
-        printf("Printing ht...\n");
-    else {
-        printf("No ht to print!\n");
-        return;
-    }
-
-    HT_ITEM_S_P *arr    = ht->arr;
-    size_t ht_len       = ht->len;
-
-    for(size_t i = 0; i < ht_len; ++i) {
-        printf("\t%zu \n", i);
-        HT_ITEM_S_P i_i = arr[i];
-        while(i_i != NULL) {
-            printf("\tkey is %s and data is %s\n", i_i->key, i_i->data);
-            i_i = i_i->next;
-        }
-    }
-}
+/*
+ * MISCELANEOUS
+ */
+ 
+/* MISCELANEOUS EXTERNAL IMPLEMENTATIONS */
 
 unsigned int seed;
 char **
@@ -349,7 +337,7 @@ fill_ht_with_r_d(HT ht, size_t size, int32_t min, int32_t max)
 
 	for(size_t i = 0; i < size; ++i) {
 		int d_n   = get_rand_num(min, max);
-		int key_n = ( (size_t)pow(get_rand_num(min, max), i) << sizeof(max) / 2) | d_n >> 2;
+		int key_n = ( (size_t)pow(get_rand_num(min, max), i) << sizeof(max) / 2) | d_n>>2;
 		
         #ifdef DEBUG
             printf("rand num is %d for key and %d for data\n", key_n, d_n);
@@ -377,6 +365,75 @@ fill_ht_with_r_d(HT ht, size_t size, int32_t min, int32_t max)
     return arr_of_keys;
 }
 
+/* MISCELANEOUS INTERNAL IMPLEMENTATIONS */
+
+static char
+is_prime(size_t x)
+{
+    for (size_t i = 3; i < x; i += 2)
+    {
+        size_t p_sqrt = x / i;
+        if (p_sqrt < i)
+            return 1;
+        if(p_sqrt * i == x)
+            return 0;
+    }
+    return 1;
+}
+
+static size_t
+next_prime(size_t x)
+{
+    #ifdef DEBUG
+        printf("\nEntering next_prime with value %zu\n", x);
+    #endif
+
+    if (x < 2)      return 2;
+
+    if (!(x & 1))   ++x;
+    else            x += 2;
+
+    for (; !is_prime(x); x += 2)    ;
+
+    #ifdef DEBUG
+        printf("\tNext prime is %zu\n\n", x);
+    #endif
+
+    return x;
+}
+
+static size_t
+hash(char *key, uint32_t n)
+{
+    uint_least64_t pre_h = 0;
+    prehash(key, &pre_h);
+
+    #ifdef DEBUG
+        printf("\tprehash value is %" PRIuLEAST64 ".\n", pre_h);
+    #endif
+
+    return pre_h % n;
+}
+
+static void
+prehash(char *key, uint_least64_t *prehash)
+{
+    size_t key_len      = strlen(key);
+    uint_least64_t sum  = 0;
+    char prev           = 0;
+    char next           = 0;
+    size_t i            = 0;
+    for( ; i < key_len - 1; ++i) {
+        next = key[i+1];
+        if(i % 2 == 0)  sum += (key[i] + prev) * next; 
+        else            sum += key[i] * prev + next;
+        prev = key[i];
+    }
+    sum += key[i] + sum & next;
+
+    *prehash            = sum;
+}
+
 static void
 init_rand(void) 
 {
@@ -387,60 +444,4 @@ static int32_t
 get_rand_num(int32_t l, int32_t h)
 {
 	return l + rand_r(&seed) % h;
-}
-
-int main(void)
-{
-    new_ht(ht,1);
-
-    size_t size = 100;
-    char ** arr = fill_ht_with_r_d(ht, size, 0, size);
-
-    print_ht(ht);
-
-    for(size_t i = 0; i < size; ++i) {
-        if(arr[i] != NULL) {
-            //HT_ITEM_S_P item = pop_item(ht, arr[i]);
-            HT_ITEM_S_P item = find_item_by_key(ht, arr[i]);
-            printf("poped item data is %s and key is %s\n", item->data, item->key);
-            /*
-            free(item->data);
-            free(item->key);
-            free(item);
-            */
-        }
-    }
-
-    print_ht(ht);
-    
-    delete_ht(ht);
-    print_ht(ht);
-
-    free(arr);
-/*
-    char *s1 = "lalala";
-    char *s2 = "alalal";
-    char *s5 = "lllaaa";
-    char *s3 = "tsaet";
-    char *s4 = "teast";
-    char *s6 = "etats";
-
-    insert_data(ht, s1, strlen(s1), s1);
-    insert_data(ht, s2, strlen(s2), s2);
-    insert_data(ht, s3, strlen(s3), s3);
-    insert_data(ht, s4, strlen(s4), s4);
-    insert_data(ht, s5, strlen(s5), s5);
-    insert_data(ht, s6, strlen(s6), s6);
-
-    print_ht(ht);
-
-    pop_item(ht, s1);
-    print_ht(ht);
-
-    pop_item(ht, s2);
-    print_ht(ht);
-
-    pop_item(ht, s3);
-    print_ht(ht);
-*/
 }
--- datastructures/hash/hash_table/ht_list.h
+++ datastructures/hash/hash_table/ht_list.h
@@ -1,40 +1,32 @@
 #ifndef HT_H_SENTRY
 #define HT_H_SENTRY
 
+
 #include <stddef.h>
 #include <inttypes.h>
+
 
 typedef struct ht       HT_S;
 typedef HT_S *          HT;
 typedef struct ht_item  HT_ITEM_S;
 typedef HT_ITEM_S*      HT_ITEM_S_P;
 
+#ifndef DEF_HT_SIZE
 #define DEF_HT_SIZE     8
+#endif
 
 
-#define     new_ht(ht, size)    HT ht   = init_hash_table(size)
-#define     delete_ht(ht)       ht      = delete_ht_do(ht)
+#define         new_ht(ht, size)    HT ht   = init_hash_table(size)
+HT              init_hash_table     (size_t len);
 
-signed char
-insert_into_slot(HT_ITEM_S_P *ht_arr, void *data, size_t d_size, char *key, size_t h_i);
+#define         delete_ht(ht)       ht      = delete_ht_do(ht)
+HT              delete_ht_do        (HT ht);
 
-enum { INCREASE_HT, DECREASE_HT };
-void
-change_ht_len(HT ht, int change_how);
-
-
-size_t next_prime(size_t x);
-size_t prev_prime(size_t n);
-
-
-size_t
-hash(char *key, uint32_t n);
-
-void
-prehash(char *key, uint_least64_t *prehash);
-
-
-
+signed char     insert_data         (HT ht, void *data, size_t d_size, char *key);
+HT_ITEM_S_P     find_item_by_key    (HT ht, char *key);
+HT_ITEM_S_P     pop_item            (HT ht, char *key);
+void            print_ht            (HT ht);
+char **         fill_ht_with_r_d    (HT ht, size_t size, int32_t min, int32_t max);
 
 
 #endif
